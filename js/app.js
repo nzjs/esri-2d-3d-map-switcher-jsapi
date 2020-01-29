fe5e27e435af93166445e0192a82a14655af08e6
John Stowell <john.stowell@wcrc.govt.nz>
2020-01-29 15:09:03 +1300

    Fix dialog and legend issue

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@
 require([
     "esri/WebScene", "esri/WebMap", "esri/views/MapView", "esri/views/SceneView",
     "esri/Graphic", "esri/widgets/Locate", "esri/widgets/Home", "esri/widgets/Search", 
-    "esri/widgets/BasemapToggle", "esri/widgets/Legend", "esri/widgets/Expand", "dijit/Dialog", "dojo/domReady!"
+    "esri/widgets/BasemapToggle", "esri/widgets/Legend", "esri/widgets/Expand", "dijit/ConfirmDialog", "dojo/domReady!"
     ], function (
     WebScene, WebMap, MapView, SceneView,
     Graphic, Locate, Home, Search, BasemapToggle,
-    Legend, Expand, Dialog) {
+    Legend, Expand, ConfirmDialog) {
 
     function addWidgets(view) {
         // Define our widgets for the view
@@ -41,16 +41,12 @@ require([
         const legendWidget = new Legend({
         view: view
         })
-        const legendExpand = new Expand({
-        view: view,
-        content: legendWidget
-        })
         // Add widgets to the current view
         view.ui.add(locateWidget, "top-left");
         view.ui.add(homeWidget, "top-left");
         view.ui.add(searchWidget, "top-right");
         view.ui.add(basemapWidget, "bottom-left");
-        view.ui.add(legendExpand, "top-left")
+        view.ui.add(legendWidget, "bottom-right")
     }
 
     const webmap = new WebMap({
@@ -130,7 +126,7 @@ require([
     }
 
 
-    splashContent = "Welcome to the official website for the West Coast Civil Defence & Emergency Management (CDEM) Group Tsunami Evacuation Zones geospatial viewer. West Coast CDEM Group does not warrant its accuracy and disclaims all liability whatsoever for any error, inaccuracy or incompleteness of the information. No person should rely on any information within this viewer without seeking independent and professional advice.<br/><br/>"+
+    splashContent = "<a href='#'></a>Welcome to the official website for the West Coast Civil Defence & Emergency Management (CDEM) Group Tsunami Evacuation Zones geospatial viewer. West Coast CDEM Group does not warrant its accuracy and disclaims all liability whatsoever for any error, inaccuracy or incompleteness of the information. No person should rely on any information within this viewer without seeking independent and professional advice.<br/><br/>"+
     "The following terms along with the disclaimer and privacy policy serve as the agreement governing the visitors use of this website that may also be referred to as ‘the site’. The parties to this agreement include and are limited to the West Coast Civil Defence & Emergency Management Group which we may refer to as “we” or “us” or “our” and the visitor to the site, who we may refer to as ‘you’.<br/><br/>"+
     "<strong>Website Contents & Terms of Use Information</strong><br/><br/>"+
     "We hope that this website provides you with the information that you require on the West Coast Tsunami Evacuation Zones and the Tsunami Evacuation information and messaging.<br/><br/>"+
@@ -139,10 +135,10 @@ require([
     "The West Coast CDEM Group are in the process of upgrading these evacuation zones as there are many uncertainties involved including knowledge of potential tsunami sources, source characteristics, bathymetry and topography, tsunami propagation and inundation characteristics. For these reasons the data provided cannot be relied on for any reason other response and recovery planning.<br/><br/>"+
     "All viewers of these zones need to be aware that the New Zealand Coastal Policy Statement (Department of Conservation 2010) sets out national policy under the Resource Management Act 1991 (RMA) for managing areas potentially affected by coastal hazards, which includes the potential effects of tsunami, and how to avoid or mitigate them. However other than evacuation planning, the current West Coast Evacuation Zones supplied by the West Coast CDEM Group cannot be used for any other purpose such as requirements for subdivision, development, land-use and tsunami risk assessment.<br/><br/>"+
     "Additional to the inaccuracy and uncertainty of the provided zones, there is a lot of uncertainty around how tsunamis will behave, particularly large tsunamis, as we have not experienced many of these in our short-written history. Every tsunami is different depending on its source, the direction it is arriving from, and the sea state and tide at the time. There is no one tsunami that would inundate an entire zone. Rather, the zones represent an ‘envelope’ around many different possible tsunami scenarios. Inevitably this will lead to some degree of over-evacuation in any tsunami event. <br/><br/>"+
-    "Should you have questions please contact the West Coast CDEM Group at info@westcoastemergency.govt.nz .<br/><br/>"+
+    "Should you have questions please contact the West Coast CDEM Group at info@westcoastemergency.govt.nz <br/><br/>"+
     "Last updated: 18th November 2019";
 
-    const splash = new Dialog({
+    const splash = new ConfirmDialog({
         title: "Introduction & Disclaimer",
         content: splashContent,
         style: "width: 600px"
